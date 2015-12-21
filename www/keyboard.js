aab6b28894c55fa319a2c1ca28840ef8d74ec48a
Matt Long <matthewhl@gmail.com>
2015-12-21 12:27:27 -0500

    remove show and hide

--- www/keyboard.js
+++ www/keyboard.js
@@ -43,7 +43,6 @@ Keyboard.prototype.disableScrollingInShrinkView = function(disable) {
 Keyboard.prototype.fireOnShow = function() {
     this.isVisible = true;
     cordova.fireWindowEvent('keyboardDidShow');
-    this.show();
     if(this.onshow) {
     	this.onshow();
     }
@@ -52,7 +51,6 @@ Keyboard.prototype.fireOnShow = function() {
 Keyboard.prototype.fireOnHide = function() {
     this.isVisible = false;
     cordova.fireWindowEvent('keyboardDidHide');
-    this.hide();
     if(this.onhide) {
     	this.onhide();
     }
